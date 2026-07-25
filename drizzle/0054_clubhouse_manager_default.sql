37efe65a9100ed0f706b67b73179d0f2d101e27f
David Capener <davylpool@googlemail.com>
2026-07-25 19:12:38 +0100

    fix: separate clubhouse theme migration statements

--- drizzle/0054_clubhouse_manager_default.sql
+++ drizzle/0054_clubhouse_manager_default.sql
@@ -1,6 +1,8 @@
 ALTER TABLE "fkh_users"
 ALTER COLUMN "theme" SET DEFAULT 'clubhouse';
 
+--> statement-breakpoint
+
 UPDATE "fkh_users"
 SET "theme" = 'clubhouse', "updated_at" = now()
 WHERE "theme" = 'system';
